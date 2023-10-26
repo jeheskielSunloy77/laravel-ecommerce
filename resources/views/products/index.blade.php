94dbee0415cabce7927ffc3d77bfd903205afa81
jeheskielSunloy77 <jeheskielventiokysunloy@gmail.com>
2023-10-26 21:00:33 +0700

    feat: redesign product cards

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,19 +3,15 @@
 @section('title', 'Products List')
 
 @section('content')
-<div class="grid 2xl:grid-cols-4 grid-cols-4 gap-2">
+<div class="grid 2xl:grid-cols-4 grid-cols-4 gap-4">
     @foreach($products as $product)
-    <a href="{{ url('/products/' . $product->id ) }}" class="hover:shadow-md overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
-        <div class="px-4 py-2">
-            <h1 class="text-xl font-bold text-gray-800 uppercase dark:text-white">{{$product->name}}</h1>
-            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400 line-clamp-2">{{$product->description}}</p>
-        </div>
-
-        <img class="object-cover w-full h-48 mt-2" src="{{$product->image}}" alt="{{$product->name}}">
-
-        <div class="flex items-center justify-between px-4 py-2 bg-gray-900">
-            <h1 class="text-lg font-bold text-white">${{$product->price}}</h1>
-            <button class="px-2 py-1 text-xs font-semibold text-gray-900 uppercase transition-colors duration-300 transform bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">Add to cart</button>
+    <a href="{{ url('/products/' . $product->id ) }}">
+        <div class="pb-4 border border-black shadow-[4px_4px_#000] hover:shadow-[6px_6px_#000] transition-shadow">
+            <img class="object-cover w-full h-48" src="{{$product->image}}" alt="{{$product->name}}">
+            <div class="mt-2 px-4">
+                <h3 class="text-xl text-gray-800 uppercase dark:text-white leading-3">{{$product->name}}</h3>
+                <span class="text-lg font-bold">$ {{$product->price}}</span>
+            </div>
         </div>
     </a>
     @endforeach
