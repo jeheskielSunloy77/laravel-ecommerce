96fa7d0b71149f605946e55b01e60cf71a8f6022
jeheskielSunloy77 <jeheskielventiokysunloy@gmail.com>
2023-10-26 16:59:01 +0700

    chore: move all seeders to one

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,18 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Product::query()->delete();
+
+        for ($i = 1; $i <= 10; $i++) {
+            Product::create([
+                'id' => fake()->uuid(),
+                'name' => fake()->sentence(2),
+                'price' => fake()->randomFloat(2, 1, 10),
+                'description' => fake()->paragraph(20),
+                'image' => fake()->imageUrl(640, 480,),
+                'created_at' => fake()->dateTimeBetween('-1 year', 'now'),
+            ]);
+        }
     }
 }
