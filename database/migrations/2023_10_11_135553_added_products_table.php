99bc06fd008236a669f40133c6f34e5a255daa4f
jeheskielSunloy77 <jeheskielventiokysunloy@gmail.com>
2023-10-28 01:22:10 +0700

    fix: table uuid primary key

--- database/migrations/2023_10_11_135553_added_products_table.php
+++ database/migrations/2023_10_11_135553_added_products_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -12,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table): void {
-            $table->uuid('id')->primary();
+            $table->uuid('id')->default(DB::raw('(UUID())'))->primary();
             $table->string('name', 100);
             $table->decimal('price', 8, 2);
             $table->text('description');
--- database/migrations/2023_10_12_112706_create_cart_table.php
+++ database/migrations/2023_10_12_112706_create_cart_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
 
@@ -13,8 +14,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('carts', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->unsignedBigInteger('user_id');
+            $table->uuid('id')->default(DB::raw('(UUID())'))->primary();
+            $table->uuid('user_id');
             $table->uuid('product_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
