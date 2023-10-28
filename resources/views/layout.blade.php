fda88decb858c4f5cede5d097e0315b9a4aec780
jeheskielSunloy77 <jeheskielventiokysunloy@gmail.com>
2023-10-28 14:04:29 +0700

    chore: minor redesign

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -37,12 +37,12 @@ $cart= $user ? $user -> carts : null;
             </div>
             <div class="relative flex items-center w-1/3">
                 <span class="absolute">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="w-6 h-6 mx-3">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 ml-3" viewBox="0 0 24 24">
+                        <path fill="currentColor" d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5q0-2.725 1.888-4.612T9.5 3q2.725 0 4.612 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3l-1.4 1.4ZM9.5 14q1.875 0 3.188-1.313T14 9.5q0-1.875-1.313-3.188T9.5 5Q7.625 5 6.312 6.313T5 9.5q0 1.875 1.313 3.188T9.5 14Z" />
                     </svg>
                 </span>
                 <form class="w-full block">
-                    <input name="search" type="text" placeholder="Search for products" class="w-full block py-2.5 text-gray-700 placeholder-gray-400/70 bg-amber-50 border-2 border-black pl-11 pr-5 rtl:pr-11 rtl:pl-5 dark:bg-amber-50 dark:text-gray-300 dark:border-gray-600" value="{{request('search')}}">
+                    <input name="search" type="text" placeholder="Search for products" class="w-full shadow-[4px_4px_black] focus:shadow-[6px_6px_black] focus:outline-none transition-shadow block py-2.5 text-gray-700 placeholder-gray-400/70 bg-amber-50 border-2 border-black pl-11 pr-5 rtl:pr-11 rtl:pl-5 dark:bg-amber-50 dark:text-gray-300 dark:border-gray-600" value="{{request('search')}}">
                 </form>
             </div>
 
@@ -82,7 +82,7 @@ $cart= $user ? $user -> carts : null;
                                 <a href="/profile" class="block px-4 py-2 hover:bg-lime-300 border-y border-transparent hover:border-black">Profile</a>
                             </li>
                             <li>
-                                <a href="/wishlist" class="block px-4 py-2 hover:bg-lime-300 border-y border-transparent hover:border-black">Wishlist</a>
+                                <a href="/wishlists" class="block px-4 py-2 hover:bg-lime-300 border-y border-transparent hover:border-black">Wishlists</a>
                             </li>
                             <li>
                                 <a href="/transactions" class="block px-4 py-2 hover:bg-lime-300 border-y border-transparent hover:border-black">Transactions</a>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,9 +7,9 @@
     @foreach($products as $product)
     <a href="{{ url('/products/' . $product->id ) }}">
         <div class="pb-4 border border-black shadow-[4px_4px_#000] hover:shadow-[6px_6px_#000] transition-shadow">
-            <img class="object-cover w-full h-48" src="{{$product->image}}" alt="{{$product->name}}">
+            <img class="object-cover w-full h-48" src="{{$product->image}}" alt="{{$product->name}}" loading="lazy">
             <div class="mt-2 px-4">
-                <h3 class="text-xl text-gray-800 uppercase dark:text-white leading-3">{{$product->name}}</h3>
+                <h3 class="text-lg text-gray-800 uppercase dark:text-white leading-4 line-clamp-1">{{$product->name}}</h3>
                 <span class="text-lg font-bold">$ {{$product->price}}</span>
             </div>
         </div>
