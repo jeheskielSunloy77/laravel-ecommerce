7b7fa6b6ea902a6da917189540d62df9ec5d17b1
jeheskielSunloy77 <jeheskielventiokysunloy@gmail.com>
2023-11-03 15:49:54 +0700

    fix: input-error component

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,5 @@
 @section('title', 'Login')
 
-@php
-$isError=!$errors->isEmpty();
-@endphp
-
 <x-app-layout isGuest>
     <form method="POST" action="{{ route('login') }}" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4 border border-black shadow-[4px_4px_black] w-1/4">
         @csrf
@@ -15,9 +11,8 @@ $isError=!$errors->isEmpty();
                     <a href="{{route('register')}}" class="font-semibold text-lime-600 hover:underline">register</a> first.
                 </p>
             </div>
-            <x-text-field label="Email" type="email" id="email" name="email" required placeholder="someone@email.com" />
-            <x-text-field label="Password" type="password" id="password" name="password" required placeholder="xxxxxxxxxxxxxx" />
-            <x-input-error :messages="$errors->get('email') + $errors->get('password')" />
+            <x-text-field label="Email" type="email" id="email" name="email" required placeholder="someone@email.com" :errors="$errors->get('email')"/>
+            <x-text-field label="Password" type="password" id="password" name="password" required placeholder="xxxxxxxxxxxxxx" :errors="$errors->get('password')" />
             <div class="flex items-center justify-between">
                 <label for="remember-me" class="inline-flex items-center gap-2">
                     <input id="remember-me" type="checkbox" class="w-4 h-4" name="remember">
--- resources/views/components/text-field.blade.php
+++ resources/views/components/text-field.blade.php
@@ -1,10 +1,11 @@
 @props(['errors', 'label' => '', 'type' => 'text'])
+
 <div class="flex flex-col">
     <x-input-label for="{{$attributes['id']}}">
         {{$label}}
     </x-input-label>
     <x-text-input {{$attributes->except('label')}} />
-    @if($errors && !$errors->isEmpty())
+    @if(isset($errors))
     <x-input-error :messages="$errors" />
     @endif
 </div>
