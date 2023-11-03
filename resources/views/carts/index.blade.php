104c675a2d9db1a3b8774bb1a2dda1af6e57654b
jeheskielSunloy77 <jeheskielventiokysunloy@gmail.com>
2023-11-03 20:31:46 +0700

    chore: redesign carts and wishlist index

--- resources/views/carts/index.blade.php
+++ resources/views/carts/index.blade.php
@@ -17,21 +17,22 @@
                         </h3>
                     </a>
                     <div class="flex items-center gap-4 text-lg">
-                        <span>
-                            <span class="text-red-500">$</span> {{ $cart->product->price }}
-                        </span>
-                        @if($isEditing)
-                        <form action="{{ url('/carts/' . $cart->id ) }}" method="post">
-                            @csrf
-                            @method('PUT')
-                            <input type="number" name="quantity" value="{{ $cart->quantity }}" class="w-20 border border-black rounded-sm px-2 bg-amber-100">
-                            <button type="submit" class="border border-black rounded-sm px-4 hover:shadow-[4px_4px_black] hover:bg-lime-300 transition-all">Change</button>
-                        </form>
-                        @else
-                        <span>
-                            {{ $cart->quantity }}<span class="text-red-500">x</span>
-                        </span>
-                        @endif
+                        <span> <span class="text-red-700 font-bold">$</span> {{$cart->product->price}}</span>
+                        <div class="flex items-center gap-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-red-700" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M7 22q-.825 0-1.413-.588T5 20q0-.825.588-1.413T7 18q.825 0 1.413.588T9 20q0 .825-.588 1.413T7 22Zm10 0q-.825 0-1.413-.588T15 20q0-.825.588-1.413T17 18q.825 0 1.413.588T19 20q0 .825-.588 1.413T17 22ZM6.15 6l2.4 5h7l2.75-5H6.15ZM5.2 4h14.75q.575 0 .875.513t.025 1.037l-3.55 6.4q-.275.5-.738.775T15.55 13H8.1L7 15h12v2H7q-1.125 0-1.7-.988t-.05-1.962L6.6 11.6L3 4H1V2h3.25l.95 2Zm3.35 7h7h-7Z" />
+                            </svg>
+                            @if($isEditing)
+                            <form action="{{ url('/carts/' . $cart->id ) }}" method="post">
+                                @csrf
+                                @method('PUT')
+                                <input type="number" name="quantity" value="{{ $cart->quantity }}" class="w-20 border border-black rounded-sm px-2 bg-amber-100">
+                                <button type="submit" class="border border-black rounded-sm px-4 hover:shadow-[4px_4px_black] hover:bg-lime-300 transition-all">Change</button>
+                            </form>
+                            @else
+                            {{ $cart->quantity }} pcs
+                            @endif
+                        </div>
                     </div>
                 </div>
                 <div class="flex items-center gap-1 flex-col">
--- resources/views/wishlists/index.blade.php
+++ resources/views/wishlists/index.blade.php
@@ -10,7 +10,15 @@
                             {{ $wishlist->product->name }}
                         </h3>
                     </a>
-                    <span class="text-lg text-red-700">$</span> {{ $wishlist->product->price }}
+                    <div class="flex items-center gap-4 text-lg">
+                        <span> <span class="text-red-700 font-bold">$</span> {{$wishlist->product->price}}</span>
+                        <div class="flex items-center gap-1 capitalize">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-red-700" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M6.5 11L12 2l5.5 9h-11Zm11 11q-1.875 0-3.188-1.313T13 17.5q0-1.875 1.313-3.188T17.5 13q1.875 0 3.188 1.313T22 17.5q0 1.875-1.313 3.188T17.5 22ZM3 21.5v-8h8v8H3ZM17.5 20q1.05 0 1.775-.725T20 17.5q0-1.05-.725-1.775T17.5 15q-1.05 0-1.775.725T15 17.5q0 1.05.725 1.775T17.5 20ZM5 19.5h4v-4H5v4ZM10.05 9h3.9L12 5.85L10.05 9ZM12 9Zm-3 6.5Zm8.5 2Z" />
+                            </svg>
+                            {{ $wishlist->product->category }}
+                        </div>
+                    </div>
                 </div>
                 <div class="flex items-center gap-1 flex-col">
                     <form action="{{ url('/wishlists/' . $wishlist->id ) }}" method="post">
