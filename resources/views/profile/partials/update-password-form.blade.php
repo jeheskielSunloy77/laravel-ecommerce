f3b4cdfc55188df3632a000672a943dda0c50d68
jeheskielSunloy77 <jeheskielventiokysunloy@gmail.com>
2023-11-06 10:22:50 +0700

    feat: using sweetalert on status update in profile

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -20,9 +20,11 @@
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
 
-            @if (session('status') === 'password-updated')
-            <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)" class="text-sm text-gray-600 dark:text-gray-400">{{ __('Saved.') }}</p>
-            @endif
         </div>
     </form>
 </section>
+@if (session('status') === 'password-updated')
+<script>
+    swal("Password Updated", "Your password has been successfully updated. please use your new password next time you login.", "success");
+</script>
+@endif
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -43,10 +43,11 @@
 
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
-
-            @if (session('status') === 'profile-updated')
-            <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)" class="text-sm text-gray-600 dark:text-gray-400">{{ __('Saved.') }}</p>
-            @endif
         </div>
     </form>
 </section>
+@if (session('status') === 'profile-updated')
+<script>
+    swal("Profile Updated", "Your profile has been successfully updated.", "success");
+</script>
+@endif
