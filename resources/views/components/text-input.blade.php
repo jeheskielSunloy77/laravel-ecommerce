db2b576112f2110c44f91ea5e90f9c574b9f8b41
jeheskielSunloy77 <jeheskielventiokysunloy@gmail.com>
2023-11-08 23:27:45 +0700

    feat: added text area variant of text input

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,1 +1,6 @@
+@props(['isTextArea' => false])
+@if($isTextArea)
+<textarea {{$attributes->merge(['class'=>'px-4 py-2 border border-black bg-transparent placeholder:text-gray-500 outline-none focus:shadow-[4px_4px_black] transition-shadow'])}}></textarea>
+@else
 <input {{$attributes->merge(['class'=>'px-4 py-2 border border-black bg-transparent placeholder:text-gray-500 outline-none focus:shadow-[4px_4px_black] transition-shadow'])}}>
+@endif
