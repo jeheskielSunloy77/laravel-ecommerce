f597d08c193e3f640153a2373ebfff10cc9a5634
jeheskielSunloy77 <jeheskielventiokysunloy@gmail.com>
2023-11-09 17:28:53 +0700

    fix: text area component not having default value

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,6 +1,8 @@
 @props(['isTextArea' => false])
 @if($isTextArea)
-<textarea {{$attributes->merge(['class'=>'px-4 py-2 border border-black bg-transparent placeholder:text-gray-500 outline-none focus:shadow-[4px_4px_black] transition-shadow'])}}></textarea>
+<textarea {{$attributes->except('value')->merge(['class'=>'px-4 py-2 border border-black bg-transparent placeholder:text-gray-500 outline-none focus:shadow-[4px_4px_black] transition-shadow'])}}>
+{{$attributes['value']}}
+</textarea>
 @else
 <input {{$attributes->merge(['class'=>'px-4 py-2 border border-black bg-transparent placeholder:text-gray-500 outline-none focus:shadow-[4px_4px_black] transition-shadow'])}}>
 @endif
