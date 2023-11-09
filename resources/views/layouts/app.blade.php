15e6c3a23f30e95de277b00ebbfd7eaf37be1429
jeheskielSunloy77 <jeheskielventiokysunloy@gmail.com>
2023-11-09 17:39:35 +0700

    feat: added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.svg') }}" type="image/x-icon">
 
     <title>@yield('title')</title>
 
