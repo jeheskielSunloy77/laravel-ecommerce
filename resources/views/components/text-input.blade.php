6948bc3a039e6c83fbbfea80849727859ed02784
jeheskielSunloy77 <jeheskielventiokysunloy@gmail.com>
2023-11-10 12:57:42 +0700

    fix: text area placeholder not showing

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,8 +1,6 @@
 @props(['isTextArea' => false])
 @if($isTextArea)
-<textarea {{$attributes->except('value')->merge(['class'=>'px-4 py-2 border border-black bg-transparent placeholder:text-gray-500 outline-none focus:shadow-[4px_4px_black] transition-shadow'])}}>
-{{$attributes['value']}}
-</textarea>
+<textarea {{$attributes->except('value')->merge(['class'=>'px-4 py-2 border border-black bg-transparent placeholder:text-gray-500 outline-none focus:shadow-[4px_4px_black] transition-shadow'])}}>{{$attributes['value']}}</textarea>
 @else
 <input {{$attributes->merge(['class'=>'px-4 py-2 border border-black bg-transparent placeholder:text-gray-500 outline-none focus:shadow-[4px_4px_black] transition-shadow'])}}>
 @endif
