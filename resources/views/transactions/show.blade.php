a5719255d86768ec6bfb9cce81fc33e0990f52d1
jeheskielSunloy77 <jeheskielventiokysunloy@gmail.com>
2023-11-10 13:30:34 +0700

    fix: user can update given product review

--- resources/views/transactions/show.blade.php
+++ resources/views/transactions/show.blade.php
@@ -91,7 +91,9 @@ $sessionStatus = session('status');
                 <x-primary-button>View Product</x-primary-button>
             </a>
             <a href="{{ route('transactions.show',['transaction'=>$transaction->id,'review'=>'true']) }}">
-                <x-secondary-button>Give Review</x-secondary-button>
+                <x-secondary-button>
+                    {{$transaction->rating ? 'See Review' : 'Give Review'}}
+                </x-secondary-button>
             </a>
             @if(request('review')==='true')
             <x-modal name="product-review" :show="true" focusable>
@@ -110,16 +112,16 @@ $sessionStatus = session('status');
                         <x-input-label for="rating">
                             Rating
                         </x-input-label>
-                        <input type="number" class="hidden" id="rating" name="rating" value="{{$transaction->rating}}">
-                        <span class="flex items-center text-red-700">
-                            @for($i=0; $i<5;$i++) <svg id="rating-star-{{$i+1}}" xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 cursor-pointer hover:text-red-600" viewBox="0 0 24 24">
+                        <input type="number" class="hidden" id="rating" name="rating" value="{{$transaction->rating}}" :disabled="$transaction->rating">
+                        <span class="flex items-center text-red-700 {{$transaction->rating ? 'cursor-not-allowed' : 'cursor-pointer' }}">
+                            @for($i=0; $i<5;$i++) <svg id="rating-star-{{$i+1}}" xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 hover:text-red-600" viewBox="0 0 24 24">
                                 <path fill="currentColor" d="{{ $transaction->rating <= $i ? 'm8.85 17.825l3.15-1.9l3.15 1.925l-.825-3.6l2.775-2.4l-3.65-.325l-1.45-3.4l-1.45 3.375l-3.65.325l2.775 2.425l-.825 3.575ZM5.825 22l1.625-7.025L2 10.25l7.2-.625L12 3l2.8 6.625l7.2.625l-5.45 4.725L18.175 22L12 18.275L5.825 22ZM12 13.25Z' : 'm5.825 22l1.625-7.025L2 10.25l7.2-.625L12 3l2.8 6.625l7.2.625l-5.45 4.725L18.175 22L12 18.275L5.825 22Z' }}" />
                                 </svg>
                                 @endfor
                         </span>
                         <x-input-error :messages="$errors->get('rating')" class="mt-1" />
                     </div>
-                    <x-text-field label="Review" id="review" required name="review" placeholder="This product is very good, I like it very much. I will buy it again in the future." rows="4" :errors="$errors->get('review')" isTextArea />
+                    <x-text-field label="Review" id="review" value="{{$transaction->review}}" :disabled="$transaction->rating" required name="review" placeholder="This product is very good, I like it very much. I will buy it again in the future." rows="4" :errors="$errors->get('review')" isTextArea />
 
                     <div class="flex justify-end gap-4">
                         <a href="{{ route('transactions.show',$transaction->id) }}">
@@ -133,6 +135,7 @@ $sessionStatus = session('status');
                     </div>
                 </form>
             </x-modal>
+            @if(!$transaction->rating)
             <script>
                 const rating = document.getElementById('rating');
                 const ratingStars = document.querySelectorAll('[id^="rating-star-"]');
@@ -148,6 +151,7 @@ $sessionStatus = session('status');
                     });
                 });
             </script>
+            @endif
             @endif
         </section>
     </div>
